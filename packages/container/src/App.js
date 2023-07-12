0dd1ba8cf99757ca579da6234ccca37de556f879
nikolov9996 <zhivko.nikolov@yara.com>
2023-07-12 18:01:34 +0300

    history between MFS

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+  seed: "ddd444",
+});
 
 export default () => {
-  return <div>
-    <h1>container</h1>
-    <hr/>
-    <MarketingApp />
-  </div>
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <MarketingApp />
+      </BrowserRouter>
+    </StylesProvider>
+  );
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 import App from "./app";
 
-const mount = (el) => {
-  ReactDOM.render(<App/>, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({pathname:nextPathname}){
+     const {pathname } = history.location;
+
+     if( pathname !== nextPathname){
+      history.push(nextPathname)
+     }
+    }
+  }
 };
 
 if (process.env.NODE_ENV === "development") {
@@ -15,6 +32,4 @@ if (process.env.NODE_ENV === "development") {
 } else {
 }
 
-export  {
-  mount
-}
+export { mount };
