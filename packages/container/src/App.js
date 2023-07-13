61cb13e452102773c1a3e32c7916f3ad05de6999
nikolov9996 <zhivko.nikolov@yara.com>
2023-07-13 15:51:42 +0300

    added dashboard MF

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,29 +1,57 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useState, useEffect } from "react";
 import Header from "./components/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Router,
+  Redirect,
+} from "react-router-dom";
+import { createBrowserHistory } from "history";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import Progress from "./components/Progress";
 
 const AuthApp = lazy(() => import("./components/AuthApp"));
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "ma",
   seed: "ddd444",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
-        <Header />
+      <Router history={history}>
+        <Header
+          isSignedIn={isSignedIn}
+          onSignOut={() => setIsSignedIn(false)}
+        />
         <Suspense fallback={<Progress />}>
           <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
+            <Route path="/auth">
+              <AuthApp onSignIn={() => setIsSignedIn(true)} />
+            </Route>
+            <Route path="/dashboard">
+              {!isSignedIn ? <Redirect to={"/"} /> : <DashboardApp />}
+            </Route>
+            <Route path="/">
+              <MarketingApp />
+            </Route>
           </Switch>
         </Suspense>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -54,11 +54,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Header({ signedIn, onSignOut }) {
+export default function Header({ isSignedIn, onSignOut }) {
   const classes = useStyles();
 
   const onClick = () => {
-    if (signedIn && onSignOut) {
+    if (isSignedIn && onSignOut) {
       onSignOut();
     }
   };
@@ -82,14 +82,14 @@ export default function Header({ signedIn, onSignOut }) {
             App
           </Typography>
           <Button
-            color="primary"
+            color={isSignedIn ? 'secondary' : 'primary'}
             variant="outlined"
             className={classes.link}
             component={RouterLink}
-            to={signedIn ? '/' : '/auth/signin'}
+            to={isSignedIn ? '/' : '/auth/signin'}
             onClick={onClick}
           >
-            {signedIn ? 'Logout' : 'Login'}
+            {isSignedIn ? 'Logout' : 'Login'}
           </Button>
         </Toolbar>
       </AppBar>
